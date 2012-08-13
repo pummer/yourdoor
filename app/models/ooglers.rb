8a9820b90b5e45dff01e05385264e67bd34bec2e
= <andy.block@gmail.com>
2012-08-12 22:51:44 -0400

    modified ooglers associations

--- app/models/ooglers.rb
+++ app/models/ooglers.rb
@@ -1,3 +1,4 @@
 class Ooglers < ActiveRecord::Base
   attr_accessible :email, :mobile_phone, :name
+  has_many :ooglers_types
 end
--- app/models/r_app.rb
+++ app/models/r_app.rb
@@ -1,7 +1,6 @@
 class RApp < ActiveRecord::Base
   attr_accessible  :submission_date
   has_many :i_apps
-  has_many :r_app_ooglers
   has_one :property
   has_one :realtor, :through => :r_app_ooglers, :conditions => ["ooglers_type.name = Realtor"]
   has_one :landlord, :through => :r_app_ooglers, :conditions => ["ooglers_type.name = Landlord"]
