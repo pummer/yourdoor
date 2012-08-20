1d8a309f47c4a88146c10ea53ec54d6c11e84e65
pummer <adampumm@gmail.com>
2012-08-20 16:55:54 -0500

    commenting out email validation for now

--- app/models/ooglers.rb
+++ app/models/ooglers.rb
@@ -2,7 +2,7 @@ class Ooglers < ActiveRecord::Base
   attr_accessible :email, :mobile_phone, :name
   
   validates_presence_of :name
-  validates :email, :presence => true, :email => true
+  # validates :email, :presence => true, :email => true
   validates_format_of :phone, 
                     :with => /\A[0-9]{10}\Z/, 
                     :allow_blank => true, 
