53d3f3fadc78d3e289cf127e1030afda5d4566ab
Roly203 <roly203@gmail.com>
2012-08-26 21:22:40 -0500

    Accounts need to be confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825181557) do
+ActiveRecord::Schema.define(:version => 20120827020916) do
 
   create_table "emergency_contacts", :force => true do |t|
     t.string   "name"
@@ -165,8 +165,12 @@ ActiveRecord::Schema.define(:version => 20120825181557) do
     t.datetime "updated_at",                             :null => false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
